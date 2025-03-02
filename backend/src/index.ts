073a18b647cf91a023183248153142021a4056fb
vadym <vadymroshuor@gmail.com>
2025-03-01 19:58:29 -0800

    integration with MapBox

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,37 @@
 import express from 'express';
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = 3001;
 
-app.get('/', (req: any, res: { send: (arg0: string) => void; }) => {
-    res.send('Hello, World!');
+app.get('/address', async (req:any, res:any) => {
+    const query = req.query.query;
+    if (!query) {
+        return res.status(400).json({ error: 'Query parameter is required' });
+    }
+
+    try {
+        const response = await axios.get(
+            `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json`,
+            {
+                params: {
+                    access_token: process.env.MAPBOX_API_KEY,
+                },
+            }
+        );
+
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch address data' });
+    }
 });
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
+
+
+
